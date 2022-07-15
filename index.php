965e3310bdacc8227e9f47de5853ef532695a900
Ben LeClair <leclben@gmail.com>
2022-07-15 09:28:34 -0400

    creating web index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Comments</h1>  
+    <h3>Candy</h3>
+    <div></div>    
+    <h3>Call me / Don't call me</h3>
+    <div></div>    
+    <h3>Who referred me</h3>
+    <div></div>	
+    <h3>Signature requirements upon delivery</h3>
+    <div></div>
+    <h3>Miscellaneous comments (everything else)</h3>
+    <div></div>
+</body>
